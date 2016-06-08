6e089bca0de8f3d6cbc09d0c2553a7bfb799444e
Brent Schavee <bhs@raven.ravenind.net>
2016-06-07 19:54:09 -0500

    initial code commit 2

---
+++ AzureWebJobExample/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Nop.Integration.WebJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public const string QueueName = "MyQueue";
+
+        public static void ProcessQueueMessage([ServiceBusTrigger(QueueName)] BrokeredMessage message, TextWriter log)
+        {
+            log.WriteLine("Scheduled job fired!");
+            log.WriteLine(message);
+            log.WriteLine("Body: " + message.GetBody<string>());
+            log.WriteLine("MessageID: " + message.MessageId);
+
+            try
+            {
+                //Do some work
+
+                message.Complete();
+            }
+            catch (Exception)
+            {
+                log.WriteLine("There was an issue processing this message. Abandoning message.");
+
+                // Indicates a problem, unlock message in queue. Makes available to be recieved again.
+                message.Abandon();
+            }
+        }
+    }
+}
---
+++ AzureWebJobExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Nop.Integration.WebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        private static string _servicesBusConnectionString;
+        private static NamespaceManager _namespaceManager;
+
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        public static void Main()
+        {
+            _servicesBusConnectionString = AmbientConnectionStringProvider.Instance.GetConnectionString(ConnectionStringNames.ServiceBus);
+            _namespaceManager = NamespaceManager.CreateFromConnectionString(_servicesBusConnectionString);
+            CreateStartMessage();
+
+            JobHostConfiguration config = new JobHostConfiguration();
+            ServiceBusConfiguration serviceBusConfig = new ServiceBusConfiguration
+            {
+                ConnectionString = _servicesBusConnectionString
+            };
+            config.UseServiceBus(serviceBusConfig);
+
+            var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+
+        private static void CreateStartMessage()
+        {
+            if (!_namespaceManager.QueueExists(Functions.QueueName))
+            {
+                _namespaceManager.CreateQueue(Functions.QueueName);
+            }
+
+            QueueClient queueClient = QueueClient.CreateFromConnectionString(_servicesBusConnectionString, Functions.QueueName);
+
+            using (Stream stream = new MemoryStream())
+            using (TextWriter writer = new StreamWriter(stream))
+            {
+                writer.Write("Start");
+                writer.Flush();
+                stream.Position = 0;
+
+                queueClient.Send(new BrokeredMessage(stream) { ContentType = "text/plain" });
+            }
+
+            queueClient.Close();
+        }
+    }
+}
